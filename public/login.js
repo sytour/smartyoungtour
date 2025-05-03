9f4fcd8764742255f574b633b11dfb8cc5f44d46
sytour <dshfiyge@gmail.com>
2025-05-03 10:52:32 +0900

    Delete public directory

--- public/login.js
+++
@@ -1,9 +0,0 @@
-function login() {
-  const username = document.getElementById("username").value.trim();
-  const password = document.getElementById("password").value.trim();
-  if (username === "agent" && password === "1234") {
-    window.location.href = "user_info_form.html";
-  } else {
-    alert("아이디 또는 비밀번호가 올바르지 않습니다.");
-  }
-}
