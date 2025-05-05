1f2b82a1f9dadba7cda8b43f8be716dba233dc1b
sytour <dshfiyge@gmail.com>
2025-05-05 15:11:59 +0900

    Update admin_hotel_price_manage.js

--- public/admin_hotel_price_manage.js
+++ public/admin_hotel_price_manage.js
@@ -1,5 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDEoEvrhTfLaqtqR1Bva_pIbskWl5Ah0CE",
