3864b3ab991cd3cf583264ef22d0a4186f034fbb
sytour <dshfiyge@gmail.com>
2025-05-19 14:44:51 +0900

    Add files via upload

---
+++ public/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCxO9V7d3DpBiD8IsNn0S8jDmh5AHSaMC4",
+  authDomain: "smartyoungtour.firebaseapp.com",
+  projectId: "smartyoungtour",
+  storageBucket: "smartyoungtour.appspot.com",
+  messagingSenderId: "153288737640",
+  appId: "1:153288737640:web:b6e6f2588f5def46d85f9a"
+};
